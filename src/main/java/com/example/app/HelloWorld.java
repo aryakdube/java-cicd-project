8454b3d8b3a6d5660becf0641c81753727cbb35a
aryakdube <aryakdubey89@gmail.com>
2025-06-07 21:33:14 +0530

    Add .gitignore to exclude build artifacts

---
+++ src/main/java/com/example/app/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example.app;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("It's me Aryak! My Java CI/CD is working.");
+    }
+}
